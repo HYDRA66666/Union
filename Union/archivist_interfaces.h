3758d25ad1217c474ba7927e6c6e976e2a458587
HYDRA15 <1048946361@qq.com>
2025-10-27 18:37:01 +0800

    更新

--- Union/archivist_interfaces.h
+++ Union/archivist_interfaces.h
@@ -7,6 +7,7 @@
 namespace HYDRA15::Union::archivist
 {
     /******************************* 数据层接口 *******************************/
+    // 数据层：组织存储数据、管理磁盘io、提供表访问接口
     // 字段信息
     class field_spec
     {
@@ -31,7 +32,7 @@ namespace HYDRA15::Union::archivist
         // 获取、写入记录项
     public:
         virtual types::field at(field_spec) = 0;
-        virtual entry set(field_spec, const types::field&) = 0;
+        virtual std::shared_ptr<entry> set(field_spec, const types::field&) = 0;
 
         // 信息接口
     public:
@@ -62,22 +63,18 @@ namespace HYDRA15::Union::archivist
         // 增删改查接口
     public:
         // 查询返回表记录的引用，应当可以通过引用修改表项
-        virtual entry at(types::ID) = 0;                                                               // 通过 ID 查询
-        virtual std::list<entry> at(std::function<bool(const entry&)>) = 0;     // 通过过滤器查找
+        virtual std::shared_ptr<entry> at(types::ID) = 0;                                   // 通过 ID 查询
+        virtual std::list<std::shared_ptr<entry>> at(std::function<bool(const entry&)>) = 0;// 通过过滤器查找
 
         // 信息和控制接口
     public:
-        virtual types::ID load(types::ID begin, types::ID end) = 0; // 从磁盘加载指定范围的数据
-        virtual types::ID sync(types::ID begin, types::ID end) = 0; // 将指定范围的数据写入磁盘，但是不改变内存中的数据
-        virtual types::ID flush(types::ID begin, types::ID end) = 0;// 将指定范围的数据写入磁盘，并删除内存中对应的数据
-
         virtual types::ID size() = 0;   // 返回表记录条数
         virtual size_t memsize() = 0;   // 返回表所占内存大小
 
         // 迭代器接口
     public:
-        virtual entry begin() = 0;
-        virtual entry end() = 0;
+        virtual std::shared_ptr<entry> begin() = 0;
+        virtual std::shared_ptr<entry> end() = 0;
 
         // 表锁
     public:
